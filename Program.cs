225ab0636ef88cfc0c14828f254dce33370ed27d
tmbnrt <120473193+tmbnrt@users.noreply.github.com>
2025-06-20 10:56:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please note: This software is proprietary. This repository serves solely as description and does not include the source code.");
+		Console.ReadLine();
+	}
+}
